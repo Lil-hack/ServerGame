6b4a7e36b28330435a449de1dfb0f0c5a4b85f22
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-01-27 11:31:37 +0000

    7
    7

--- Serverside Code/MainRoom Code/Game.cs
+++ Serverside Code/MainRoom Code/Game.cs
@@ -46,7 +46,7 @@ namespace MushroomsUnity3DExample {
             foreach (Player pl in Players) {
 				if(pl.ConnectUserId != player.ConnectUserId) {
 					pl.Send("PlayerJoined", player.ConnectUserId, 0, 0,0);
-					player.Send("PlayerJoined", pl.ConnectUserId, pl.posx,pl.posy, pl.posz);
+					player.Send("PlayerJoined", pl.ConnectUserId, pl.posx,pl.posy, pl.posz,pl.skin);
 				}
 			}
 
