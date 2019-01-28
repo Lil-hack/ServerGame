0c21f61b7f29e21629d314c1eee89e9df41ab4e8
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-01-28 01:18:18 +0000

    990
    890

--- Serverside Code/WalkRoom Code/Game.cs
+++ Serverside Code/WalkRoom Code/Game.cs
@@ -8,7 +8,8 @@ namespace MushroomsUnity3DExample {
 		 public float posy = 0;
         public float posz = 0;
         public float roty = 0;
-        public int skin = 0;
+        public float skin = 0;
+
 
         public int toadspicked = 0;
     }
@@ -22,7 +23,7 @@ namespace MushroomsUnity3DExample {
 
     [RoomType("WalkTypeOne")]
 	public class GameCode : Game<Player> {
-        private int countPlayerWalk=0;
+      
         private int last_toad_id = 0;
         private List<Toad> Toads = new List<Toad>();
         // This method is called when an instance of your the game is created
@@ -92,15 +93,24 @@ namespace MushroomsUnity3DExample {
 
 		// This method is called whenever a player joins the game
 		public override void UserJoined(Player player) {
-			foreach(Player pl in Players) {
-				if(pl.ConnectUserId != player.ConnectUserId) {
-					pl.Send("PlayerJoined", player.ConnectUserId, 0, 0,0);
-					player.Send("PlayerJoined", pl.ConnectUserId, pl.posx,pl.posy, pl.posz);
-				}
-			}
+            player.posx = (float)Convert.ToDouble(player.JoinData["posx"]);
+            player.posy = (float)Convert.ToDouble(player.JoinData["posy"]);
+            player.posz = (float)Convert.ToDouble(player.JoinData["posz"]);
+            player.roty = (float)Convert.ToDouble(player.JoinData["roty"]);
+            player.skin = (float)Convert.ToDouble(player.JoinData["skin"]);
 
-			// send current toadstool info to the player
-			foreach(Toad t in Toads) {
+            foreach (Player pl in Players)
+            {
+
+                if (pl.ConnectUserId != player.ConnectUserId)
+                {
+                    pl.Send("PlayerJoined", player.ConnectUserId, player.posx, player.posy, player.posz, player.roty, player.skin);
+                    player.Send("PlayerJoined", pl.ConnectUserId, pl.posx, pl.posy, pl.posz, pl.roty, Convert.ToDouble(pl.JoinData["skin"]));
+                }
+            }
+
+            // send current toadstool info to the player
+            foreach (Toad t in Toads) {
 				player.Send("Toad", t.id, t.posx, t.posz);
 			}
 		}
@@ -110,51 +120,35 @@ namespace MushroomsUnity3DExample {
 			Broadcast("PlayerLeft", player.ConnectUserId);
 		}
 
-        public override bool AllowUserJoin(Player player)
-        {
-            if (PlayerCount >= 20)
-            {
-                player.Send("Error", 0);
-                return false;
-            }
-            else
-                return true;
-        }
+      //  public override bool AllowUserJoin(Player player)
+       // {
+         //   if (PlayerCount >= 20)
+           // {
+            //    player.Send("Error", 0);
+            //    return false;
+           // }
+           // else
+             //   return true;
+      //  }
         // This method is called when a player sends a message into the server code
         public override void GotMessage(Player player, Message message) {
 			switch(message.Type) {
-				// called when a player clicks on the ground
-				case "Move":
-					player.posx = message.GetFloat(0);
+                // called when a player clicks on the ground
+                case "Move":
+                    player.posx = message.GetFloat(0);
                     player.posy = message.GetFloat(1);
                     player.posz = message.GetFloat(2);
-           
-                    Broadcast("Move", player.ConnectUserId, player.posx, player.posy, player.posz);
-					break;
-               
+                    player.roty = message.GetFloat(3);
+                    Broadcast("Move", player.ConnectUserId, player.posx, player.posy, player.posz, player.roty);
+                    break;
+
                 case "Rotation":
 
                     player.roty = message.GetFloat(0);
                     Broadcast("Rotation", player.ConnectUserId, player.roty);
                     break;
 
-                case "Skin":
-                    player.skin = message.GetInt(0);
-                    Broadcast("Skin", player.ConnectUserId, player.skin);
-                    break;
-
-                case "ComeInGame":
-                    if (countPlayerWalk >= 1)
-                    {
-                        player.Send("Error", 1);
-                    }
-                    else {
-                     countPlayerWalk++;
-                        player.Send("OpenDoor");
-                    }
-                   
-                    Broadcast("Skin", player.ConnectUserId, player.skin);
-                    break;
+     
                 case "Pickup":
                     // called when the player is actually close to the harvesting node
                     int pickupid = int.Parse(message.GetString(0).Replace("Toad", ""));
